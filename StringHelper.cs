d8eb5014a937cfd6439a1a7205fa7f2fb5d80e49
dopelord24 <47433928+dopelord24@users.noreply.github.com>
2019-02-17 10:09:32 +0200

    Add files via upload

---
+++ StringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PadawansTask4
+{
+    public class StringHelper
+    {
+        public static int GetVowelCount(string str)
+        {
+            str.ToLower;
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+                if (str.Contains("a") || str.Contains("e") || str.Contains("i") || str.Contains("o") || str.Contains("u"))
+                {
+                    count++;
+                }
+            return count;
+        }
+    }
+}
